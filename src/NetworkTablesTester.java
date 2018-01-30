5951736adaedcc84b0718bf1313eca9ce404f17a
Bradley-B <brad.boxer1@gmail.com>
2018-01-30 15:40:53 -0500

    Higher Dimensions and Better Displays
    Changed the code to accept Waypoint[][] and output Trajectory[] so we
    can have multiple paths generated. Also moved the display out to a
    different class in case we need to change it later.

--- src/NetworkTablesTester.java
+++ src/NetworkTablesTester.java
@@ -17,7 +17,7 @@ public class NetworkTablesTester {
 	public static final double maxVel = 10;
 	public static final double maxAccel = 8;
 	public static final double maxJerk = 60;
-	
+
 	/**
 	 * This will actually be the roboRio
 	 * @param args
@@ -27,66 +27,70 @@ public class NetworkTablesTester {
 		NetworkTable.setServerMode();
 		NetworkTable.setTeam(303);
 		NetworkTable.initialize();
-		
+
 		NetworkTable pathfinderInputTable = NetworkTable.getTable("pathfinderInput");
 		NetworkTable pathfinderOutputTable = NetworkTable.getTable("pathfinderOutput");		
-		
+
 		pathfinderOutputTable.addTableListener(new ITableListener() {
 			@Override
 			public void valueChanged(ITable table, String string, Object recievedObject, boolean newValue) {
 				System.out.println("[server] String: "+string+" Value: "+recievedObject+" new: "+newValue);
 			}
 		});
-		
+
 		pathfinderInputTable.putNumber("timeStep", timeStep);
 		pathfinderInputTable.putNumber("maxVel", maxVel);
 		pathfinderInputTable.putNumber("maxAccel", maxAccel);
 		pathfinderInputTable.putNumber("maxJerk", maxJerk);
-		
-		//while(!Thread.interrupted()) {
-			
-		pathfinderInputTable.putString("waypoints", serializeWaypointArray(new Waypoint[] {
+
+		pathfinderInputTable.putString("waypoints", serializeWaypointArray2d(new Waypoint[][] {
+			{//this is waypoints[0], and will output to trajectories[0]
+				new Waypoint(0, 0, 0), 
+				new Waypoint(0, 0, 0), //this point is waypoints[0, 1]
+				new Waypoint(0, 0, 0)
+			}, {
+				new Waypoint(0, 0, 0), //this point is waypoints[1, 0]
+				new Waypoint(0, 0, 0)
+			}, {//this is waypoints[2] and will output to trajectories[2]
 				new Waypoint(0, 0, 0),
-				new Waypoint(1, 0, 0),
-				new Waypoint(9, 0, 0),
-				new Waypoint(10, 0, 0)
+				new Waypoint(0, 0, 0),
+				new Waypoint(0, 0, 0)
+			}
 		}));
 
 		NetworkTable.flush();
-	
-		//}
-		
+
 		while (!Thread.interrupted()) {
 			Thread.sleep(100);
 		}
-		
+
 	}
 
-	public static Trajectory deserializeTrajectory(String serializedTrajectory) {
-		Trajectory trajectory = null; 
+	public static Trajectory[] deserializeTrajectoryArray(String serializedTrajectoryArray) {
+		Trajectory[] trajectories = null; 
 		try {
-			 byte[] b = Base64.getDecoder().decode(serializedTrajectory.getBytes()); 
-			 ByteArrayInputStream bi = new ByteArrayInputStream(b);
-		     ObjectInputStream si = new ObjectInputStream(bi);
-		     trajectory = (Trajectory) si.readObject();
-		 } catch (Exception e) {
-		     e.printStackTrace();
-		 }
-		return trajectory;
+			byte[] b = Base64.getDecoder().decode(serializedTrajectoryArray.getBytes()); 
+			ByteArrayInputStream bi = new ByteArrayInputStream(b);
+			ObjectInputStream si = new ObjectInputStream(bi);
+			trajectories = (Trajectory[]) si.readObject();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return trajectories;
 	}
-	
-	public static String serializeWaypointArray(Waypoint[] waypoints) {
-		 String serializedWaypoints = "";
-		 try {
-		     ByteArrayOutputStream bo = new ByteArrayOutputStream();
-		     ObjectOutputStream so = new ObjectOutputStream(bo);
-		     so.writeObject(waypoints);
-		     so.flush();
-		     serializedWaypoints = new String(Base64.getEncoder().encode(bo.toByteArray()));
-		 } catch (Exception e) {
-		     e.printStackTrace();
-		 }
+
+	public static String serializeWaypointArray2d(Waypoint[][] waypoints2d) {
+		String serializedWaypoints = "";
+		try {
+			ByteArrayOutputStream bo = new ByteArrayOutputStream();
+			ObjectOutputStream so = new ObjectOutputStream(bo);
+			so.writeObject(waypoints2d);
+			so.flush();
+			serializedWaypoints = new String(Base64.getEncoder().encode(bo.toByteArray()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return serializedWaypoints;
 	}
-	
+
 }
--- src/RemotePathGenerator.java
+++ src/RemotePathGenerator.java
@@ -17,45 +17,23 @@ import jaci.pathfinder.Pathfinder;
 import jaci.pathfinder.Trajectory;
 import jaci.pathfinder.Waypoint;
 
-public class RemotePathGenerator extends JFrame implements ITableListener, IRemoteConnectionListener {
+public class RemotePathGenerator implements ITableListener, IRemoteConnectionListener {
 
-	private static final long serialVersionUID = -3371908382388429620L;
 	NetworkTable pathfinderInputTable;
 	NetworkTable pathfinderOutputTable;
 	boolean connected = false;
 	int genID = 1;
-
-	/*static {
-		System.loadLibrary("pathfinderjava.dll");
-	}*/
-	static {
-	  //  try {
-	    	System.load("C:\\Users\\Bradley\\workspace\\RemotePathGenerator\\pathfinderjava.dll");
-//	    } catch (UnsatisfiedLinkError e) {
-//	      System.err.println("Native code library failed to load.\n" + e);
-//	      System.exit(1);
-//	    }
-	  }
+	DisplayFrame display;
 	
 	public static void main(String[] args) {
 		NetworkTable.setClientMode();
 		NetworkTable.setTeam(303);
-		NetworkTable.setIPAddress("192.168.43.158"); //NT server address - will be RoboRIO's. TODO
+		NetworkTable.setIPAddress("127.0.0.1"); //NT server address - will be RoboRIO's.
 		new RemotePathGenerator();
 	}
 
 	public RemotePathGenerator() {
-		this.setTitle("Remote Path Generator Status");
-		this.setSize(450, 300);
-		this.getContentPane().setBackground(Color.RED);
-		this.setAlwaysOnTop(true);
-		this.setUndecorated(true);
-		this.getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
-		//this.setOpacity(0.5f);
-		this.setLocationRelativeTo(null);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setVisible(true);
-
+		display = new DisplayFrame();
 		pathfinderInputTable = NetworkTable.getTable("pathfinderInput");
 		pathfinderOutputTable = NetworkTable.getTable("pathfinderOutput");
 		pathfinderInputTable.addTableListener(this, true);
@@ -64,76 +42,85 @@ public class RemotePathGenerator extends JFrame implements ITableListener, IRemo
 
 	@Override
 	public void valueChanged(ITable iTable, String string, Object recievedObject, boolean newValue) {
-		System.out.println("[client] String: "+string+" Value: "+recievedObject+" new: "+newValue);
-
-		this.getContentPane().setBackground(Color.YELLOW);
+		System.out.println("[client recieved] String: "+string+" Value: "+recievedObject+" new: "+newValue);
 
 		if(string.equals("waypoints")) {
-
-			Waypoint[] waypoints = deserializeWaypointArray((String)recievedObject);
+			display.setBackground(Color.YELLOW);
+			Waypoint[][] waypoints2d = deserializeWaypointArray2d((String)recievedObject);
 			double timeStep = pathfinderInputTable.getNumber("timeStep", 0.05);
 			double maxVel = pathfinderInputTable.getNumber("maxVel", 10);
 			double maxAccel  = pathfinderInputTable.getNumber("maxAccel", 20);
 			double maxJerk = pathfinderInputTable.getNumber("maxJerk", 30);
-			Trajectory trajectory = generatePath(waypoints, timeStep, maxVel, maxAccel, maxJerk);
+			
+			Trajectory[] trajectories = generatePaths(waypoints2d, timeStep, maxVel, maxAccel, maxJerk);
 
 			//output results to NT
 			//genID is used for verification that the data is new, since if the same path object is output then valueChanged() doesn't fire.
-			pathfinderOutputTable.putValue("path", serializeTrajectory(trajectory));
+			pathfinderOutputTable.putValue("path", serializeTrajectoryArray(trajectories));
 			pathfinderOutputTable.putValue("ID", genID);
+			System.out.println("client sent data and genID "+genID);
 			genID++;
 		}
 
-		this.getContentPane().setBackground(Color.GREEN);
+		if(!display.getBackground().equals(Color.BLACK)) { 
+			display.setBackground(Color.GREEN);
+		}
+		
 	}
 
-	public Trajectory generatePath(Waypoint[] waypoints, double timeStep, double maxVel, double maxAccel, double maxJerk) {
-		Trajectory forwardTrajectory = null;
-		try {	
-			Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH,
-					timeStep, maxVel, maxAccel, maxJerk);
-			forwardTrajectory = Pathfinder.generate(waypoints, config);	
-		} catch (Exception e) {}
-		return forwardTrajectory;
+	public Trajectory[] generatePaths(Waypoint[][] waypoints2d, double timeStep, double maxVel, double maxAccel, double maxJerk) {
+		Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH,
+				timeStep, maxVel, maxAccel, maxJerk);
+		
+		//first dimension of waypoints2d is what will be a trajectory, the second dimension is the waypoints in that trajectory
+		Trajectory[] trajectories = new Trajectory[waypoints2d.length];
+		for(int i=0;i<trajectories.length;i++) {
+			try {
+				Waypoint[] waypoints = waypoints2d[i];
+				trajectories[i] = Pathfinder.generate(waypoints, config);
+			} catch (Exception e) {display.setBackground(Color.BLACK);}
+		}
+		
+		return trajectories;
 	}
 
-	public String serializeTrajectory(Trajectory trajectory) {
-		String serializedTrajectory = ""; 
+	public String serializeTrajectoryArray(Trajectory[] trajectoryArray) {
+		String serializedTrajectoryArray = ""; 
 		
 		try {
 		     ByteArrayOutputStream bo = new ByteArrayOutputStream();
 		     ObjectOutputStream so = new ObjectOutputStream(bo);
-		     so.writeObject(trajectory);
+		     so.writeObject(trajectoryArray);
 		     so.flush();
-		     serializedTrajectory = new String(Base64.getEncoder().encode(bo.toByteArray()));
+		     serializedTrajectoryArray = new String(Base64.getEncoder().encode(bo.toByteArray()));
 		 } catch (Exception e) {
 		     System.out.println(e);
 		 }
-		return serializedTrajectory;
+		return serializedTrajectoryArray;
 	}
 
-	public Waypoint[] deserializeWaypointArray(String serializedWaypoints) {
-		Waypoint[] waypoints = null; 
+	public Waypoint[][] deserializeWaypointArray2d(String serializedWaypointsArray2d) {
+		Waypoint[][] waypoints2d = null; 
 		try {
-		     byte[] b = Base64.getDecoder().decode(serializedWaypoints.getBytes()); 
+		     byte[] b = Base64.getDecoder().decode(serializedWaypointsArray2d.getBytes()); 
 		     ByteArrayInputStream bi = new ByteArrayInputStream(b);
 		     ObjectInputStream si = new ObjectInputStream(bi);
-		     waypoints = (Waypoint[]) si.readObject();
+		     waypoints2d = (Waypoint[][]) si.readObject();
 		 } catch (Exception e) {
 		     System.out.println(e);
 		 }
-		return waypoints;
+		return waypoints2d;
 	}
 	
 	@Override
 	public void connected(IRemote arg0) {
-		this.getContentPane().setBackground(Color.GREEN);
+		display.setBackground(Color.GREEN);
 		connected = true;
 	}
 
 	@Override
 	public void disconnected(IRemote arg0) {
-		this.getContentPane().setBackground(Color.RED);
+		display.setBackground(Color.RED);
 		connected = false;
 	}
 
