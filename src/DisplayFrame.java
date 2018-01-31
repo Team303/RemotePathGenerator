d783db7d00e6456889c4eb349e8d42dd86c01ebf
Bradley-B <brad.boxer1@gmail.com>
2018-01-30 23:31:57 -0500

    Fixed robbie's mistake
    Robbie deleted DisplayFrame.java, which we kind've needed for the
    program to work. So I put it back.

---
+++ src/DisplayFrame.java
@@ -0,0 +1,40 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JRootPane;
+
+public class DisplayFrame extends JFrame{
+	private static final long serialVersionUID = -924699517006012122L;
+
+	public DisplayFrame() {
+		this.setTitle("Remote Path Generator Status");
+		this.setSize(450, 300);
+		setBackground(Color.RED);
+		this.setAlwaysOnTop(true);
+		this.setUndecorated(true);
+		this.getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
+		//this.setOpacity(0.5f);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+
+	}
+
+	/**
+	 * Sets the background of the frame. Display color codes: <ul>
+	 * 		<li>RED: NT Disconnected</li> 
+	 * 		<li>GREEN: NT Connected</li> 
+	 * 		<li>YELLOW: Currently generating trajectories</li> 
+	 * 		<li>BLACK: Fatal error, one or more trajectories failed to generate</li> 
+	 */
+	public void setBackground(Color color) {
+		if(getBackground()!=Color.BLACK) {
+			this.getContentPane().setBackground(color);			
+		}
+	}
+	
+	public Color getBackground() {
+		return this.getContentPane().getBackground();
+	}
+	
+}
